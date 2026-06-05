e1eac8f9589a011b012f024db5216a4716ab40c0
ritzmosas <moritzsasowski@hotmail.com>
2026-06-05 13:01:23 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -147,7 +147,7 @@ export default function Home() {
               href="/events"
               className="indie-btn text-large md:text-large text-medium bg-[var(--accent-yellow)] text-black border-black hover:bg-[var(--accent)] hover:text-white"
             >
-              Tour Daten
+              Tourdaten
             </Link>
           </div>
           <div className="position-center space-medium">
